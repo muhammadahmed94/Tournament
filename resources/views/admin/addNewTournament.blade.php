a6a34e197018f95f827ce447207d68c822d4a504
DRHIRA\hira <rao.noman786@outlook.com>
2017-07-22 18:21:14 +0500

    last

--- resources/views/admin/addNewTournament.blade.php
+++ resources/views/admin/addNewTournament.blade.php
@@ -1,24 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />
+@extends('layouts.mainFrameAdmin')
 
-    
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-material-design/0.5.10/css/ripples.min.css"/>
-
-    <link rel="stylesheet" href="./css/bootstrap-material-datetimepicker.css" />
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,500' rel='stylesheet' type='text/css'>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/stylesheet.css" rel="stylesheet"> 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css"> 
-    <script src="https://code.jquery.com/jquery-2.2.4.js"></script>
-    <style>
+@section('bodyContentAdmin')
+<style>
         .mdl-textfield {
     padding: 24px 0;
     
@@ -92,57 +75,6 @@
   }
   
     </style>
-</head>
-<body>
-  <nav class="navbar navbar-default navbar-inverse" role="navigation">
-    <div class="container-fluid">
-      <!-- Brand and toggle get grouped for better mobile display -->
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <!-- <a class="navbar-brand" href="#">LOGO</a> -->
-      </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <ul class="nav navbar-nav">
-          <li class="dropdown "><a class="dropdown-toggle" data-toggle="dropdown" href="#">Tournaments<span class="caret"></span></a>
-            <ul class="dropdown-menu">
-              <li><a href="#">Add New Tournament</a></li>
-              <li><a href="#">Chi Town Shuffle</a></li>
-            </ul>
-          </li>
-          <li><a href="#">Development</a></li>
-          <li><a href="#">Clinics</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="#"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> <b>Admin</b> <span class="caret"></span></a>
-        <ul id="login-dp" class="dropdown-menu">
-          <li>
-            <div class="">
-                <div class="col-md-12">
-                  <center><img class="img-circle" style="width: 100px; border: 2px solid #FF8333;" src="images/img1.jpg">
-                  <h4 style="margin-top:10px; color: black;">Admin</h4>
-                  <p style="margin-top:-5px; color: black;" >Admin@hotmail.com</p></center>
-                  
-                <div class="form-group">
-                   <button style="color: white;" type="submit" class="btn btn-primary btn-block signout-btn">Sign Out</button>
-                </div>
-            </div>
-           </div>
-          </li>
-        </ul>
-          </li>
-        </ul>
-      </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-  </nav>
   <div class="container">
     <h3>Tournament Info</h3>
       <form action=" {{ url('/addNewTournamentWithPost') }}" method="Post" accept-charset="utf-8">
@@ -249,7 +181,7 @@
                 if (x < max_fields) { //max input box allowed
                     x++; //text box increment
 
-                    $(wrapper).append('<div class="after-add'+x+'"> <div class="tournament-info"> <div class="tournament-info-title"> <a href="" class="remove_field"><img style="height: 50px; padding: 10px; float: right;" src="images/cancel.png" alt=""> </a> </div><div class="col-md-2"></div><div class="col-md-4"> <div class="form-group"> <label for="usr">Division Title</label> <input type="text" class="form-control" name="Division_Title[]"id="usr"> </div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Birth Year</label> <input type="text" class="form-control" name="Birth_Year[]" id="usr"> </div></div></div><div class="col-md-2"></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Boys/Girls</label> <input type="text" class="form-control" name="Boys/Girls[]" id="usr"> </div></div></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Limit / Division</label> <input type="text" class="form-control" name="Limit[]" id="usr"> </div></div></div><div class="col-md-2"></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Level</label> <input type="text" class="form-control" name="Level[]" id="usr"> </div></div></div></div><div class="col-md-4"> <div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Entry Fee</label> <input type="text" class="form-control" name="Entry_Fee" id="usr"> </div></div></div><div class="col-md-2"></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Deposite</label> <input type="text" class="form-control" name="Deposite[]" id="usr"> </div></div></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Early Bid</label> <input type="text" class="form-control" name="Early_Bid[]" id="usr"> </div></div></div><div class="col-md-2"></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Early Bid Exp</label> <input type="text" class="form-control" name="Early_Bid_Exp[]" id="usr"> </div></div></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Multi Team</label> <input type="text" class="form-control" name="Multi_Team" id="usr"> </div></div></div><div class="col-md-2"></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Canadian</label> <input type="text" class="form-control" name="Canadian[]" id="usr"> </div></div></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Balanace Due</label> <input type="text" class="form-control" name="Balanace_Due[]" id="usr"> </div></div></div><div class="col-md-2"></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Balanace Due Date</label> <input type="text" class="form-control" name="Balanace_Due_Date[]" id="usr"> </div></div></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Balanace Team Disc</label> <input type="text" class="form-control" name="Balanace_Team_Disc[]" id="usr"> </div></div></div><div class="col-md-2"></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Balanace Canada</label> <input type="text" class="form-control" name="Balanace_Canada[]" id="usr"> </div></div></div></div><div class="col-md-2"></div></div><div style="width: 100%; float: left; margin-bottom: 30px;"> <button style="float: left;" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent add-div" data-upgraded=",MaterialButton,MaterialRipple"><img style=" height: 18px; margin-top: -5px; margin-right: 7px;" src="images/add.png">Add Division<span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span> </button> <button type="button" style="float: right;" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent clone-btn'+x+'" data-upgraded=",MaterialButton,MaterialRipple"><img style=" height: 18px; margin-top: -5px; margin-right: 7px;" src="images/copy.png">Clone<span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span> </button></div></div><div class="clone-data'+x+'"></div>');
+                    $(wrapper).append(getDivHtml(x));
                     componentHandler.upgradeDom();
 
                }
@@ -269,7 +201,10 @@
             
             });
               });
+              function getDivHtml(x){
+               
+                return '<div class="after-add'+x+'"> <div class="tournament-info"> <div class="tournament-info-title"> <a href="" class="remove_field"><img style="height: 50px; padding: 10px; float: right;" src="images/cancel.png" alt=""> </a> </div><div class="col-md-2"></div><div class="col-md-4"> <div class="form-group"> <label for="usr">Division Title</label> <input type="text" class="form-control" name="Division_Title[]"id="usr"> </div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Birth Year</label> <input type="text" class="form-control" name="Birth_Year[]" id="usr"> </div></div></div><div class="col-md-2"></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Boys/Girls</label> <input type="text" class="form-control" name="Boys/Girls[]" id="usr"> </div></div></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Limit / Division</label> <input type="text" class="form-control" name="Limit[]" id="usr"> </div></div></div><div class="col-md-2"></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Level</label> <input type="text" class="form-control" name="Level[]" id="usr"> </div></div></div></div><div class="col-md-4"> <div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Entry Fee</label> <input type="text" class="form-control" name="Entry_Fee" id="usr"> </div></div></div><div class="col-md-2"></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Deposite</label> <input type="text" class="form-control" name="Deposite[]" id="usr"> </div></div></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Early Bid</label> <input type="text" class="form-control" name="Early_Bid[]" id="usr"> </div></div></div><div class="col-md-2"></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Early Bid Exp</label> <input type="text" class="form-control" name="Early_Bid_Exp[]" id="usr"> </div></div></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Multi Team</label> <input type="text" class="form-control" name="Multi_Team" id="usr"> </div></div></div><div class="col-md-2"></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Canadian</label> <input type="text" class="form-control" name="Canadian[]" id="usr"> </div></div></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Balanace Due</label> <input type="text" class="form-control" name="Balanace_Due[]" id="usr"> </div></div></div><div class="col-md-2"></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Balanace Due Date</label> <input type="text" class="form-control" name="Balanace_Due_Date[]" id="usr"> </div></div></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Balanace Team Disc</label> <input type="text" class="form-control" name="Balanace_Team_Disc[]" id="usr"> </div></div></div><div class="col-md-2"></div><div class="col-md-5"> <div class="row"> <div class="form-group"> <label for="usr">Balanace Canada</label> <input type="text" class="form-control" name="Balanace_Canada[]" id="usr"> </div></div></div></div><div class="col-md-2"></div></div><div style="width: 100%; float: left; margin-bottom: 30px;"> <button style="float: left;" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent add-div" data-upgraded=",MaterialButton,MaterialRipple"><img style=" height: 18px; margin-top: -5px; margin-right: 7px;" src="images/add.png">Add Division<span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span> </button> <button type="button" style="float: right;" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent clone-btn'+x+'" data-upgraded=",MaterialButton,MaterialRipple"><img style=" height: 18px; margin-top: -5px; margin-right: 7px;" src="images/copy.png">Clone<span class="mdl-button__ripple-container"><span class="mdl-ripple"></span></span> </button></div></div><div class="clone-data'+x+'"></div>'
+              }
 
     </script>
-</body>
-</html>
+@endsection
